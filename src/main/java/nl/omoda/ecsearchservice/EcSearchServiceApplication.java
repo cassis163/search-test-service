9659ba5d373c4d4a2df0964f39b83a647296ae83
casper.aangeenbrug <casper.aangeenbrug@omoda.nl>
2022-06-03 12:08:14 +0200

    Initial commit

---
+++ src/main/java/nl/omoda/ecsearchservice/EcSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.omoda.ecsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcSearchServiceApplication.class, args);
+	}
+
+}
